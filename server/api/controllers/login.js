4f169595a0718e83a009f17677b92e20f8cd69e7
José Roberto Vasconcelos <joserobertovasconcelos@gmail.com>
2017-04-11 12:17:18 -0300

    Swagger com autenticacao

--- server/api/controllers/login.js
+++ server/api/controllers/login.js
@@ -5,6 +5,7 @@ var jwt    = require('jsonwebtoken');
 module.exports = {
   authenticate: (req, res)=>{
     var user = req.swagger.params.body.value.email;
+    console.log(req.body);
     console.log(user);
     var token = jwt.sign(user, '12345678');
     res.json({token:token});
--- server/app.js
+++ server/app.js
@@ -5,7 +5,13 @@ var app = require('express')();
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+  appRoot: __dirname, // required config
+  swaggerSecurityHandlers: {
+    Bearer: function (req, authOrSecDef, scopesOrApiKey, cb) {
+      console.log('Opa. Passei aqui!!!', scopesOrApiKey);
+      cb(null);
+    }
+  }
 };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
@@ -23,12 +29,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   };
   swaggerExpress.runner.swaggerTools.swaggerSecurity(options);
   */
-  swaggerExpress.runner.swaggerTools.swaggerSecurity({
-    Bearer:
-    function (req, authOrSecDef, scopesOrApiKey, callback) {
-      console.log('Opa. Passei aqui!!!');
-      callback();
-    }});
+
   var port = process.env.PORT || 8000;
   app.listen(port);
 
