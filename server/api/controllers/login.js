ff6dcf9353cd7c51650c2eedd76bd20509f0a3cf
Jose Roberto <jose.roberto@dinamonetworks.com>
2017-04-12 00:51:27 -0300

    integracao com o SCPA

--- server/api/controllers/login.js
+++ server/api/controllers/login.js
@@ -1,11 +1,35 @@
 'use strict';
 
-var jwt    = require('jsonwebtoken');
+const jwt    = require('jsonwebtoken');
+const yaml   = require('js-yaml');
+const fs   = require('fs');
+const soap = require('soap');
+const crypto = require('crypto');
 
 module.exports = {
   authenticate: (req, res)=>{
-    var user = req.swagger.params.body.value.email;
-    console.log(user);
+    var user = req.body.email;
+    var password = crypto.createHash('sha256').update(req.body.password, 'utf8').digest().toString('hex');
+
+    try {
+      var doc = yaml.safeLoad(fs.readFileSync('config/default.yaml', 'utf8'));
+      soap.createClient(doc.config.wsdl, function(err, client) {
+            //console.log('Erro:',err, client );
+            client.buscaPerfilUsuario(
+              {autenticacao: {email: user, senha: password, siglaSistema: doc.config.system}},
+              function(err, result) {
+                console.log('Err:', err);
+                console.log(result.respostaBuscaPerfilUsuario.usuario);
+                console.log(result.respostaBuscaPerfilUsuario.perfis.perfil[0].perfil.sigla,
+                  result.respostaBuscaPerfilUsuario.perfis.perfil[0].esferas.esferasPerfil[0].configuracao);
+              });
+        });
+
+      console.log();
+    } catch (e) {
+      console.log(e);
+    }
+
     var token = jwt.sign(user, '12345678');
     res.json({token:token});
   }
