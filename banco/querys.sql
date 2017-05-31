29494b306b15b294e41ac00f633f12d8fd1b9524
Jose Roberto <jose.roberto@dinamonetworks.com>
2017-05-31 03:27:57 -0300

    Busca basica sem filtro funcionando

--- banco/querys.sql
+++ banco/querys.sql
@@ -13,3 +13,5 @@ SELECT co_ibge AS munic, nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_men
 SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_grau2_deformidade FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 82 GROUP BY co_ibge,nu_ano_ref;
 
 SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_pop_geral FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 16 GROUP BY co_ibge,nu_ano_ref;
+
+SELECT tb_ibge.regiao, tb_ibge.co_uf, tb_ibge.cidade, tabpop.ano_pop as ano, sum(tabpop.pop_me_15) as pop_me_15, sum(tabpop.pop) as pop_ FROM dbgeral.tb_pop_faixas AS tabpop INNER JOIN dbgeral.tb_ibge AS tb_ibge ON tabpop.co_ibge = tb_ibge.ibge WHERE  tabpop.pop_me_15 > 0 AND tabpop.pop > 0 AND tabpop.ano_pop > 2000 group by tabpop.ano_pop,tb_ibge.cidade,tb_ibge.co_uf,tb_ibge.regiao
--- server/api/controllers/query.js
+++ server/api/controllers/query.js
@@ -3,12 +3,14 @@ const pg = require('pg');
 const _ = require('underscore');
 
 const indicador = {
+  'IBGE': { desc: 'IBGE',
+            sql:'SELECT tb_ibge.regiao, tb_ibge.ibge as munic, tb_ibge.cidade, tabpop.ano_pop as ano, sum(tabpop.pop_me_15) as pop_me_15, sum(tabpop.pop) as pop_ FROM dbgeral.tb_pop_faixas AS tabpop INNER JOIN dbgeral.tb_ibge AS tb_ibge ON tabpop.co_ibge = tb_ibge.ibge WHERE  tabpop.pop_me_15 > 0 AND tabpop.pop > 0 AND tabpop.ano_pop > 2000 group by tabpop.ano_pop,tb_ibge.cidade,tb_ibge.ibge,tb_ibge.regiao'},
   'DHM14A':{ desc: 'Detecção em menores de 15 anos',
-           sql: 'SELECT co_ibge AS munic, nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_menores_15_anos FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 10 GROUP BY co_ibge,nu_ano_ref;'},
+           sql: 'SELECT co_ibge AS munic, nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_menores_15_anos FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 10 GROUP BY co_ibge,nu_ano_ref'},
   'DGIID':{ desc: 'Detecção de grau II de deformidade',
-       sql: 'SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_grau2_deformidade FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 82 GROUP BY co_ibge,nu_ano_ref;'},
+       sql: 'SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_grau2_deformidade FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 82 GROUP BY co_ibge,nu_ano_ref'},
   'DPG': { desc:'Detecção na pop. geral',
-        sql: 'SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_pop_geral FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 16 GROUP BY co_ibge,nu_ano_ref;'}
+        sql: 'SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_pop_geral FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 16 GROUP BY co_ibge,nu_ano_ref'}
 };
 
 const config = {
@@ -29,27 +31,21 @@ pool.on('error', function (err, client) {
 
 module.exports = {
   getValores: (req, res)=>{
-    console.log(req.swagger.params.codigos.value);
-    var task = [];
+    var sql_from = ' SELECT * FROM IBGE ';
+    var sql_with = 'WITH IBGE AS (' + indicador['IBGE'].sql + ')';
     req.swagger.params.codigos.value.forEach((item)=>{
       if(indicador[item]){
-        task.push(
-          (cb)=>{
-            pool.query(indicador[item].sql,null, cb);
-          });
+        sql_with = sql_with + ',' + item + ' AS (' + indicador[item].sql + ')';
+        sql_from = sql_from + ' left JOIN ' + item + ' ON IBGE.munic=' + item + '.munic AND IBGE.ano=' + item + '.ano' ;
       }
     });
-
-    async.parallel(task, (err,result)=>{
+    pool.query(sql_with+sql_from,null, (err, result)=>{
       if(err) {
         console.error('error running query', err);
         res.json({mensagem:err});
         return;
       }
-      var mergedlist = _.map(result[0].rows, function(item){
-          return _.extend(item, _.findWhere(result[1].rows, { munic: item.munic, ano: item.ano }));
-      });
-      res.json(mergedlist);
+      res.json(result.rows);
     });
   }
 }
