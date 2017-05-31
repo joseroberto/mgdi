60efaaca822b7fce72a21286096e9687822c62fd
José Roberto Vasconcelos <joserobertovasconcelos@gmail.com>
2017-05-31 11:35:30 -0300

    Interface de busca para querys agrupadas-Prototipo

--- server/api/controllers/query.js
+++ server/api/controllers/query.js
@@ -4,13 +4,22 @@ const _ = require('underscore');
 
 const indicador = {
   'IBGE': { desc: 'IBGE',
-            sql:'SELECT tb_ibge.regiao, tb_ibge.ibge as munic, tb_ibge.cidade, tabpop.ano_pop as ano, sum(tabpop.pop_me_15) as pop_me_15, sum(tabpop.pop) as pop_ FROM dbgeral.tb_pop_faixas AS tabpop INNER JOIN dbgeral.tb_ibge AS tb_ibge ON tabpop.co_ibge = tb_ibge.ibge WHERE  tabpop.pop_me_15 > 0 AND tabpop.pop > 0 AND tabpop.ano_pop > 2000 group by tabpop.ano_pop,tb_ibge.cidade,tb_ibge.ibge,tb_ibge.regiao'},
+        sql:'SELECT tb_ibge.regiao, tb_ibge.ibge as munic, tb_ibge.cidade, tabpop.ano_pop as ano, sum(tabpop.pop_me_15) as pop_me_15, sum(tabpop.pop) as pop_ FROM dbgeral.tb_pop_faixas AS tabpop INNER JOIN dbgeral.tb_ibge AS tb_ibge ON tabpop.co_ibge = tb_ibge.ibge WHERE  tabpop.pop_me_15 > 0 AND tabpop.pop > 0 AND tabpop.ano_pop > 2000 group by tabpop.ano_pop,tb_ibge.cidade,tb_ibge.ibge,tb_ibge.regiao'},
   'DHM14A':{ desc: 'Detecção em menores de 15 anos',
-           sql: 'SELECT co_ibge AS munic, nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_menores_15_anos FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 10 GROUP BY co_ibge,nu_ano_ref'},
+        sql: 'SELECT co_ibge AS munic, nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_menores_15_anos FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 10 GROUP BY co_ibge,nu_ano_ref'},
   'DGIID':{ desc: 'Detecção de grau II de deformidade',
-       sql: 'SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_grau2_deformidade FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 82 GROUP BY co_ibge,nu_ano_ref'},
+        sql: 'SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_grau2_deformidade FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 82 GROUP BY co_ibge,nu_ano_ref'},
   'DPG': { desc:'Detecção na pop. geral',
-        sql: 'SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_pop_geral FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 16 GROUP BY co_ibge,nu_ano_ref'}
+        sql: 'SELECT co_ibge AS munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_pop_geral FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 16 GROUP BY co_ibge,nu_ano_ref'},
+  'CCAC': { desc:'Casos curados nos anos das coortes',
+        sql: 'SELECT co_ibge as munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS curados_anos_coortes FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 310 GROUP BY co_ibge,nu_ano_ref'},
+  'CNDAC': { desc:'Casos novos diagnosticados nos anos das coortes',
+        sql: 'SELECT co_ibge as munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS novos_diag_anos_coortes FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 320 GROUP BY co_ibge,nu_ano_ref'},
+  'CIR': { desc:'Contatos intradomiciliares registrados',
+        sql: 'SELECT co_ibge as munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS contatos_registrados FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 90 GROUP BY co_ibge,nu_ano_ref'},
+  'CIE': { desc:'Contatos intradomiciliares examinados',
+        sql: 'SELECT co_ibge as munic,nu_ano_ref AS ano, sum(qt_casos_agravo) AS contatos_examinados FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 80 GROUP BY co_ibge,nu_ano_ref'},
+
 };
 
 const config = {
