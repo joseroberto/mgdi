36fb3c3e21a43b735f97d7714dbb091dac10dc04
Jose Roberto <jose.roberto@dinamonetworks.com>
2017-06-01 01:32:21 -0300

    Formatacao concluida para o retorno da query de indicadores

--- server/api/controllers/query.js
+++ server/api/controllers/query.js
@@ -7,25 +7,44 @@ const indicador = {
 
   // Criterio de Analise
   'IBGEMUN': { desc: 'IBGE Municipio',
+        metadata: [
+          {colType: 'String', colName: 'Região', resumo: 'Região agregada', tipo: 'geo'},
+          {colType: 'String', colName: 'Local', resumo: 'Região desagregada', tipo: 'geo'},
+          {colType: 'String', colName: 'codigogeo', resumo: 'Código da unidade', tipo: 'id'},
+          {colType: 'Numeric', colName: 'Ano', resumo: 'Ano da ocorrência', tipo: 'ano'},
+        ],
         sql:'SELECT tb_ibge.regiao, tb_ibge.ibge as co_ibge, tb_ibge.cidade, tabpop.ano_pop as ano, sum(tabpop.pop_me_15) as pop_me_15, sum(tabpop.pop) as pop_ FROM dbgeral.tb_pop_faixas AS tabpop INNER JOIN dbgeral.tb_ibge AS tb_ibge ON tabpop.co_ibge = tb_ibge.ibge WHERE  tabpop.pop_me_15 > 0 AND tabpop.pop > 0 AND tabpop.ano_pop > 2000 TTT group by tabpop.ano_pop,tb_ibge.cidade,tb_ibge.ibge,tb_ibge.regiao'},
   'IBGEEST': { desc: 'IBGE Estadual',
-              sql:'SELECT tb_ibge.regiao, tb_ibge.co_uf, tb_ibge.uf, tabpop.ano_pop as ano, sum(tabpop.pop_me_15) as pop_me_15, sum(tabpop.pop) as pop_ FROM dbgeral.tb_pop_faixas AS tabpop INNER JOIN dbgeral.tb_ibge AS tb_ibge ON tabpop.co_ibge = tb_ibge.ibge WHERE tabpop.pop_me_15 > 0 AND tabpop.pop > 0 AND tabpop.ano_pop > 2000 TTT group by tb_ibge.regiao, tb_ibge.co_uf, tb_ibge.uf, tabpop.ano_pop'},
+        metadata: [
+          {colType: 'String', colName: 'Região', resumo: 'Região agregada', tipo: 'geo'},
+          {colType: 'String', colName: 'Local', resumo: 'Região desagregada', tipo: 'geo'},
+          {colType: 'String', colName: 'codigogeo', resumo: 'Código da unidade', tipo: 'id'},
+          {colType: 'Numeric', colName: 'Ano', resumo: 'Ano da ocorrência', tipo: 'ano'},
+        ],
+        sql:'SELECT tb_ibge.regiao, tb_ibge.co_uf, tb_ibge.uf, tabpop.ano_pop as ano, sum(tabpop.pop_me_15) as pop_me_15, sum(tabpop.pop) as pop_ FROM dbgeral.tb_pop_faixas AS tabpop INNER JOIN dbgeral.tb_ibge AS tb_ibge ON tabpop.co_ibge = tb_ibge.ibge WHERE tabpop.pop_me_15 > 0 AND tabpop.pop > 0 AND tabpop.ano_pop > 2000 TTT group by tb_ibge.regiao, tb_ibge.co_uf, tb_ibge.uf, tabpop.ano_pop'},
   // TODO: Não tem por requigão?  E nacional?
 
   // Indicadores
   'DHM14A':{ desc: 'Detecção em menores de 15 anos',
+        resumo: 'Casos novos em menores de 15 anos de idade residentes em determinado local e diagnosticados no ano da avaliação',
         sql: 'SELECT XXX nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_menores_15_anos FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 10 GROUP BY XXX nu_ano_ref'},
   'DGIID':{ desc: 'Detecção de grau II de deformidade',
+        resumo: 'Detecção de grau II de deformidade","resumo":"Casos novos com grau 2 de incapacidade física no diagnóstico, residentes em determinado local e detectados no ano da avaliação',
         sql: 'SELECT XXX nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_grau2_deformidade FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 82 GROUP BY XXX nu_ano_ref'},
   'DPG': { desc:'Detecção na pop. geral',
+        resumo: 'Número de casos novos residentes em determinado local e diagnosticados no ano da avaliação',
         sql: 'SELECT XXX nu_ano_ref AS ano, sum(qt_casos_agravo) AS deteccao_pop_geral FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 16 GROUP BY XXX nu_ano_ref'},
   'CCAC': { desc:'Casos curados nos anos das coortes',
+        resumo: 'População total residente no mesmo local e período',
         sql: 'SELECT XXX nu_ano_ref AS ano, sum(qt_casos_agravo) AS curados_anos_coortes FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 310 GROUP BY XXX nu_ano_ref'},
   'CNDAC': { desc:'Casos novos diagnosticados nos anos das coortes',
+        resumo: 'Total de casos novos de hanseníase residentes no mesmo local e diagnosticados nos anos das coortes',
         sql: 'SELECT XXX nu_ano_ref AS ano, sum(qt_casos_agravo) AS novos_diag_anos_coortes FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 320 GROUP BY XXX nu_ano_ref'},
   'CIR': { desc:'Contatos intradomiciliares registrados',
+        resumo: 'Total de contatos intradomiciliares registrados referentes aos casos novos de hanseníase residentes no mesmo local e diagnosticados no ano de avaliação',
         sql: 'SELECT XXX nu_ano_ref AS ano, sum(qt_casos_agravo) AS contatos_registrados FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 90 GROUP BY XXX nu_ano_ref'},
   'CIE': { desc:'Contatos intradomiciliares examinados',
+        resumo: 'Número de contatos intradomiciliares examinados referente aos casos novos residentes em determinado local e diagnosticados no ano da avaliação',
         sql: 'SELECT XXX nu_ano_ref AS ano, sum(qt_casos_agravo) AS contatos_examinados FROM morbi_mortalidade2.tb_casos_agravo_hanseniase WHERE co_agravo = 4 AND co_grupo_agravo = 80 GROUP BY XXX nu_ano_ref'},
 
 };
@@ -65,18 +84,27 @@ module.exports = {
     var sql_from = ' SELECT * FROM IBGE ';
     var sql_with = '';
     var campo = ''; // TODO: Colocar na configuracao do indicador
+    var tipoFiltro = '', tipoRegiao = '', codigoRegiao = '';
+    var meta = [];
     switch(tipo){
       case 'UF':
         sql_with = 'WITH IBGE AS (' + indicador['IBGEEST'].sql + ')';
         campo = 'co_uf';
+        tipoFiltro = 'nacional';
+        tipoRegiao = 'brasil';
+        meta = meta.concat(indicador['IBGEEST'].metadata);
         break;
       case 'MUN':
         sql_with = 'WITH IBGE AS (' + indicador['IBGEMUN'].sql + ')';
         campo = 'co_ibge';
+        tipoFiltro = 'estadual';
+        meta = meta.concat(indicador['IBGEMUN'].metadata);
         break;
       default:
         sql_with = 'WITH IBGE AS (' + indicador['IBGEEST'].sql + ')';
         campo = 'co_uf';
+        tipoFiltro = 'nacional';
+        meta = meta.concat(indicador['IBGEEST'].metadata);
         break;
     }
 
@@ -84,15 +112,17 @@ module.exports = {
       if(indicador[item]){
         sql_with = sql_with + ',' + item + ' AS (' + indicador[item].sql + ')';
         sql_from = sql_from + ' left JOIN ' + item + ' ON IBGE.' + campo + '=' + item + '.' + campo + ' AND IBGE.ano=' + item + '.ano' ;
+        meta.push({colType: 'Numeric', colName: indicador[item].desc, resumo: indicador[item].resumo, tipo: 'valor'});
       }
     });
 
+    // Trocas
     sql_with = sql_with.replace(new RegExp('XXX','g'), campo+',');
     sql_with = sql_with.replace(new RegExp('TTT','g'), where);
 
     var sql = sql_with+sql_from + ' ORDER BY 1,2,3,4';
 
-    console.log(sql);
+    //console.log(sql);
 
     pool.query(sql,null, (err, result)=>{
       if(err) {
@@ -100,7 +130,11 @@ module.exports = {
         res.json({mensagem:err});
         return;
       }
-      res.json(result.rows);
+      res.json({
+        resultset: result.rows,
+        info: { tipoFiltro: tipoFiltro, tipoRegiao: tipoRegiao, codigoRegiao: codigoRegiao},
+        metadata:meta
+      });
     });
   }
 }
