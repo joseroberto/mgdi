9f0d10bbe8ff29d345eafab7f66a69f307f78595
Jose Roberto <jose.roberto@dinamonetworks.com>
2017-08-02 00:34:15 -0300

    Tempo de login

--- client/src/app/shared/layout/footer/footer.component.ts
+++ client/src/app/shared/layout/footer/footer.component.ts
@@ -11,6 +11,7 @@ export class FooterComponent implements OnInit {
   private versionServer:string;
   private enviroment:string;
   private title: string;
+  private ultimo_login: string;
 
   constructor(private versionService:VersionService) {}
 
@@ -21,6 +22,29 @@ export class FooterComponent implements OnInit {
         this.title = resp.title;
         this.enviroment = resp.enviroment;
     });
+
+    let user = localStorage.getItem('currentUser');
+    if (user){
+      let item_user = JSON.parse(user);
+      this.ultimo_login = this.timeConversion(new Date().getTime() - new Date(item_user.ultimo_login).getTime());
+    }
   }
+
+  timeConversion(millisec) {
+      let seconds = (millisec / 1000);
+      let minutes = (millisec / (1000 * 60));
+      let hours = (millisec / (1000 * 60 * 60));
+      let days = (millisec / (1000 * 60 * 60 * 24));
+
+      if (seconds < 60) {
+          return seconds.toFixed(0) + " Seg";
+      } else if (minutes < 60) {
+          return minutes.toFixed(0) + " Min";
+      } else if (hours < 24) {
+          return hours.toFixed(0) + " Hrs";
+      } else {
+          return days.toFixed(0) + " Dias"
+      }
+    }
 
 }
