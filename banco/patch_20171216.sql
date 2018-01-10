3d1eac5694f7d6e49507937c68bf1ac311d52335
Jose Roberto <jose.roberto@dinamonetworks.com>
2018-01-10 10:25:07 -0200

    Criação de consulta por conjunto de TAG #36G

--- banco/patch_20171216.sql
+++ banco/patch_20171216.sql
@@ -1,39 +1,39 @@
-alter table dbmgi.tb_user_mgi add column ds_login varchar(255);
-delete from dbmgi.tb_user_mgi;
-alter table dbmgi.tb_user_mgi alter column ds_login set not null;
-alter table dbmgi.tb_user_mgi add column dt_login timestamp without time zone not null;
+alter table dbesusgestor.tb_user_mgi add column ds_login varchar(255);
+delete from dbesusgestor.tb_user_mgi;
+alter table dbesusgestor.tb_user_mgi alter column ds_login set not null;
+alter table dbesusgestor.tb_user_mgi add column dt_login timestamp without time zone not null;
 
-alter table dbmgi.tb_user_mgi add column ds_telefone varchar(100);
+alter table dbesusgestor.tb_user_mgi add column ds_telefone varchar(100);
 
---alter table dbmgi.tb_user_mgi add column ds_unidade;
---alter table dbmgi.tb_user_mgi drop column ds_unidade;
-
-
-COMMENT ON COLUMN dbmgi.tb_user_mgi.ds_login IS 'Login do usuário';
-COMMENT ON COLUMN dbmgi.tb_user_mgi.dt_inclusao IS 'Data do último login';
-COMMENT ON COLUMN dbmgi.tb_user_mgi.ds_telefone IS 'Telefone';
+--alter table dbesusgestor.tb_user_mgi add column ds_unidade;
+--alter table dbesusgestor.tb_user_mgi drop column ds_unidade;
 
 
-create unique index tb_user_mgi_ds_login on dbmgi.tb_user_mgi (ds_login);
+COMMENT ON COLUMN dbesusgestor.tb_user_mgi.ds_login IS 'Login do usuário';
+COMMENT ON COLUMN dbesusgestor.tb_user_mgi.dt_inclusao IS 'Data do último login';
+COMMENT ON COLUMN dbesusgestor.tb_user_mgi.ds_telefone IS 'Telefone';
 
-alter table dbmgi.tb_user_mgi add column co_unidade int;
-COMMENT ON COLUMN dbmgi.tb_user_mgi.ds_telefone IS 'Unidade que usuario esta vinculado';
 
-alter table dbmgi.tb_user_mgi add constraint fk_tb_user_mgi_co_unidade foreign key (co_unidade)
-references dbmgi.tb_unidade(co_unidade);
+create unique index tb_user_mgi_ds_login on dbesusgestor.tb_user_mgi (ds_login);
 
-alter table dbmgi.tb_user_mgi add column co_perfil int not null;
-alter table dbmgi.tb_user_mgi add column dt_aprovacao_perfil timestamp without time zone;
-alter table dbmgi.tb_user_mgi add column co_user_aprovacao int;
+alter table dbesusgestor.tb_user_mgi add column co_unidade int;
+COMMENT ON COLUMN dbesusgestor.tb_user_mgi.ds_telefone IS 'Unidade que usuario esta vinculado';
 
-COMMENT ON COLUMN dbmgi.tb_user_mgi.co_perfil IS 'Codigo do perfil de usuario';
-COMMENT ON COLUMN dbmgi.tb_user_mgi.dt_aprovacao_perfil IS 'Data de aprovacao do perfil';
-COMMENT ON COLUMN dbmgi.tb_user_mgi.co_user_aprovacao IS 'Usuario aprovacao perfil';
+alter table dbesusgestor.tb_user_mgi add constraint fk_tb_user_mgi_co_unidade foreign key (co_unidade)
+references dbesusgestor.tb_unidade(co_unidade);
 
-alter table dbmgi.tb_user_mgi add constraint fk_tb_user_mgi_co_user_aprovacao foreign key (co_user_aprovacao)
-references dbmgi.tb_user_mgi(co_user);
+alter table dbesusgestor.tb_user_mgi add column co_perfil int not null;
+alter table dbesusgestor.tb_user_mgi add column dt_aprovacao_perfil timestamp without time zone;
+alter table dbesusgestor.tb_user_mgi add column co_user_aprovacao int;
 
-create table dbmgi.tb_perfil(
+COMMENT ON COLUMN dbesusgestor.tb_user_mgi.co_perfil IS 'Codigo do perfil de usuario';
+COMMENT ON COLUMN dbesusgestor.tb_user_mgi.dt_aprovacao_perfil IS 'Data de aprovacao do perfil';
+COMMENT ON COLUMN dbesusgestor.tb_user_mgi.co_user_aprovacao IS 'Usuario aprovacao perfil';
+
+alter table dbesusgestor.tb_user_mgi add constraint fk_tb_user_mgi_co_user_aprovacao foreign key (co_user_aprovacao)
+references dbesusgestor.tb_user_mgi(co_user);
+
+create table dbesusgestor.tb_perfil(
     co_perfil serial not null,
     ds_sigla char(3) not null,
     ds_titulo varchar(50) not null,
@@ -42,43 +42,43 @@ create table dbmgi.tb_perfil(
     st_exige_unidade boolean not null default 'F',
     CONSTRAINT tb_perfil_pkey PRIMARY KEY (co_perfil)
 );
-COMMENT ON TABLE dbmgi.tb_perfil IS 'Armazena os possíveis perfis de usuário';
-COMMENT ON COLUMN dbmgi.tb_perfil.co_perfil IS 'Surrogate key do perfil';
-COMMENT ON COLUMN dbmgi.tb_perfil.ds_sigla IS 'Sigla do perfil';
-COMMENT ON COLUMN dbmgi.tb_perfil.ds_titulo IS 'Titulo do perfil';
-COMMENT ON COLUMN dbmgi.tb_perfil.ds_perfil IS 'Descricao do perfil';
-COMMENT ON COLUMN dbmgi.tb_perfil.st_perfil IS 'Status ativo/inativo para o perfil';
-COMMENT ON COLUMN dbmgi.tb_perfil.st_exige_unidade IS 'Flag para indicar se o perfil exige unidade vinculada';
+COMMENT ON TABLE dbesusgestor.tb_perfil IS 'Armazena os possíveis perfis de usuário';
+COMMENT ON COLUMN dbesusgestor.tb_perfil.co_perfil IS 'Surrogate key do perfil';
+COMMENT ON COLUMN dbesusgestor.tb_perfil.ds_sigla IS 'Sigla do perfil';
+COMMENT ON COLUMN dbesusgestor.tb_perfil.ds_titulo IS 'Titulo do perfil';
+COMMENT ON COLUMN dbesusgestor.tb_perfil.ds_perfil IS 'Descricao do perfil';
+COMMENT ON COLUMN dbesusgestor.tb_perfil.st_perfil IS 'Status ativo/inativo para o perfil';
+COMMENT ON COLUMN dbesusgestor.tb_perfil.st_exige_unidade IS 'Flag para indicar se o perfil exige unidade vinculada';
 
-create unique index dbmgi.ux_tb_perfil_ds_sigla on dbmgi.tb_perfil(ds_sigla);
+create unique index dbesusgestor.ux_tb_perfil_ds_sigla on dbesusgestor.tb_perfil(ds_sigla);
 
-insert into dbmgi.tb_perfil (ds_sigla, ds_titulo, ds_perfil, st_exige_unidade) values ('ADM', 'Administrador do Sistema', 'O administrador do sistema é responsável por gerenciar permissões e checar dados do sistema.', 'F');
-insert into dbmgi.tb_perfil (ds_sigla, ds_titulo, ds_perfil, st_exige_unidade) values ('GES', 'Gestor da área', 'O gestor da área é responsável por manter informações de indicadores específicos da área.', 'T');
-insert into dbmgi.tb_perfil (ds_sigla, ds_titulo, ds_perfil, st_exige_unidade) values ('EQA', 'Membro da equipe das áreas', 'Membros de equipes são responsáveis por alimentar o sistema com informações de indicadores específicas da área.', 'T');
+insert into dbesusgestor.tb_perfil (ds_sigla, ds_titulo, ds_perfil, st_exige_unidade) values ('ADM', 'Administrador do Sistema', 'O administrador do sistema é responsável por gerenciar permissões e checar dados do sistema.', 'F');
+insert into dbesusgestor.tb_perfil (ds_sigla, ds_titulo, ds_perfil, st_exige_unidade) values ('GES', 'Gestor da área', 'O gestor da área é responsável por manter informações de indicadores específicos da área.', 'T');
+insert into dbesusgestor.tb_perfil (ds_sigla, ds_titulo, ds_perfil, st_exige_unidade) values ('EQA', 'Membro da equipe das áreas', 'Membros de equipes são responsáveis por alimentar o sistema com informações de indicadores específicas da área.', 'T');
 
 
-alter table dbmgi.tb_user_mgi add constraint fk_tb_user_mgi_co_perfil foreign key (co_perfil)
-  references dbmgi.tb_perfil(co_perfil);
+alter table dbesusgestor.tb_user_mgi add constraint fk_tb_user_mgi_co_perfil foreign key (co_perfil)
+  references dbesusgestor.tb_perfil(co_perfil);
 
-alter table dbmgi.tb_user_mgi add column co_situacao_perfil int not null default 0;
-COMMENT ON COLUMN dbmgi.tb_user_mgi.co_situacao_perfil IS 'Situação da aprovação da conta de usuário';
+alter table dbesusgestor.tb_user_mgi add column co_situacao_perfil int not null default 0;
+COMMENT ON COLUMN dbesusgestor.tb_user_mgi.co_situacao_perfil IS 'Situação da aprovação da conta de usuário';
 
-alter table dbmgi.tb_user_mgi alter column dt_login set default now();
+alter table dbesusgestor.tb_user_mgi alter column dt_login set default now();
 
-create table dbmgi.tb_status_aprovacao(
+create table dbesusgestor.tb_status_aprovacao(
     co_status int not null,
     ds_status varchar(255) not null,
     CONSTRAINT tb_status_aprovacao_pkey PRIMARY KEY (co_status)
 );
-COMMENT ON TABLE dbmgi.tb_status_aprovacao IS 'Armazena os possíveis status de aprovação de fluxo';
-COMMENT ON COLUMN dbmgi.tb_status_aprovacao.co_status IS 'Surrogate key do status';
-COMMENT ON COLUMN dbmgi.tb_status_aprovacao.ds_status IS 'Descrição do status';
+COMMENT ON TABLE dbesusgestor.tb_status_aprovacao IS 'Armazena os possíveis status de aprovação de fluxo';
+COMMENT ON COLUMN dbesusgestor.tb_status_aprovacao.co_status IS 'Surrogate key do status';
+COMMENT ON COLUMN dbesusgestor.tb_status_aprovacao.ds_status IS 'Descrição do status';
 
-insert into dbmgi.tb_status_aprovacao (co_status, ds_status) values (0, 'Em análise');
-insert into dbmgi.tb_status_aprovacao (co_status, ds_status) values (1, 'Aprovado');
-insert into dbmgi.tb_status_aprovacao (co_status, ds_status) values (2, 'Negado');
+insert into dbesusgestor.tb_status_aprovacao (co_status, ds_status) values (0, 'Em análise');
+insert into dbesusgestor.tb_status_aprovacao (co_status, ds_status) values (1, 'Aprovado');
+insert into dbesusgestor.tb_status_aprovacao (co_status, ds_status) values (2, 'Negado');
 
-alter table dbmgi.tb_user_mgi add constraint fk_tb_user_mgi_co_situacao foreign key (co_situacao_perfil)
-  references dbmgi.tb_status_aprovacao(co_status);
+alter table dbesusgestor.tb_user_mgi add constraint fk_tb_user_mgi_co_situacao foreign key (co_situacao_perfil)
+  references dbesusgestor.tb_status_aprovacao(co_status);
 
-alter table dbmgi.tb_user_mgi add column co_sexo char(1) not null;
+alter table dbesusgestor.tb_user_mgi add column co_sexo char(1) not null;
