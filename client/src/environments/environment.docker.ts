5d4105ef0ff324cbcbf56bf7c7ce4c162d970150
José Roberto Corrêa de Vasconcelos <jose.roberto@dinamonetworks.com>
2019-12-03 11:04:36 -0300

    Automacao construção de imagem

---
+++ client/src/environments/environment.docker.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  url: 'URL_REST',
+  API_URL: "CONTEXT_REST",
+  aplicacao: "APP", // codigo da aplicacao SIGIN
+  urlNewUser: "http://",
+  urlForgetPassword: "http://",
+  urlAuth: "URL_AUTH"
+};
