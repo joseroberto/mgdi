f590a34236a5733ecfe3704ca429c1c3ca30acf8
José Roberto Corrêa de Vasconcelos <jose.roberto@dinamonetworks.com>
2020-01-03 19:19:51 -0300

    Fix campo aplicacao vazio

--- server/api/controllers/perfil.js
+++ server/api/controllers/perfil.js
@@ -2,16 +2,19 @@ var models  = require('../models');
 
 module.exports = {
   getPerfis: (req, res)=>{
-    console.log('Teste:', req.swagger.params.aplicacao.value)
-    models.Perfil.findAll({
-      include:[{ model: models.Aplicacao,
-          as: 'Aplicacao', where:{ sigla: req.swagger.params.aplicacao.value }}],
-      where:{
-        ativo:true
-      }
-    }).then(function(lista) {
-      res.json({perfis: lista});
-    });
+    if(req.swagger.params.aplicacao.value){
+      models.Perfil.findAll({
+        include:[{ model: models.Aplicacao,
+            as: 'Aplicacao', where:{ sigla: req.swagger.params.aplicacao.value }}],
+        where:{
+          ativo:true
+        }
+      }).then(function(lista) {
+        res.json({perfis: lista});
+      });
+    }else{
+      res.json({});
+    }
   },
   getPerfilPorSigla: async (sigla)=>{
     return models.Perfil.findAll({
