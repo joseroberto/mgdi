71c0991d28b8952e13251ed55b1f0a9276aab4b2
José Roberto Corrêa de Vasconcelos <jose.roberto@dinamonetworks.com>
2020-01-15 09:11:09 -0300

    Fix tratamento para retornar apenas as UFs válida

---
+++ banco/patch_20200113.sql
@@ -0,0 +1,4 @@
+insert into dbesusgestor.tb_unidade_medida values (11, 'Número Decimal');
+update dbesusgestor.tb_unidade_medida set ds_unidade_medida='Número Inteiro' where co_unidade_medida=2;
+
+
--- server/api/controllers/uf.js
+++ server/api/controllers/uf.js
@@ -1,8 +1,9 @@
-var models  = require('../models');
+const models  = require('../models');
 
 module.exports = {
   getUf: (req, res)=>{
     models.Uf.findAll({
+      where: { codigo: {$gt: 0, $lt: 90}}
     }).then(function(lista) {
       res.json(lista);
     });
