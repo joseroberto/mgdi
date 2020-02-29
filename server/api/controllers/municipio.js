f6cfaf738bc81c737377066d5254ff48e88d02e3
Jose Roberto <joserobertovasconcelos@gmail.com>
2020-02-29 13:45:35 -0300

    Acrescenta um parametro de consulta municipios por UF

--- server/api/controllers/municipio.js
+++ server/api/controllers/municipio.js
@@ -2,8 +2,15 @@ var models  = require('../models');
 
 module.exports = {
   getMunicipio: (req, res)=>{
-    models.Municipio.findAll({
-    }).then(function(lista) {
+    let query = {
+      include: [ {model: models.Uf, as: 'UF'}]
+    }
+    if(req.swagger.params.uf.value){
+      query={
+        include: [ {model: models.Uf, as: 'UF', where:{sigla: req.swagger.params.uf.value.toUpperCase()}}]
+      }
+    }
+    models.Municipio.findAll(query).then(function(lista) {
       res.json(lista);
     });
   }
