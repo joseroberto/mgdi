9c1e1b58dc4900e327cef807e11131e07c7e243f
Luiz Oliveira Montedonio <montedonio.luiz@gmail.com>
2021-03-12 15:17:12 -0300

    fix tabela indicadores

--- client/src/app/services/acl.service.ts
+++ client/src/app/services/acl.service.ts
@@ -9,62 +9,63 @@ var acl = require("../helpers/acl-module/index.js");
 
 @Injectable()
 export class AclService extends REST {
-  private role:string;
-  private userUnidadeCod:string;
+  private role: string;
+  private userUnidadeCod: string;
   private userUnidadesPath;
 
-  constructor(http:Http) {
+  constructor(http: Http) {
 
     super(http);
     let acl = JSON.parse(localStorage.getItem('acl'));
-    let role = _.get(JSON.parse(localStorage.getItem('currentUser')),'Perfil.sigla','guest');
+    let role = _.get(JSON.parse(localStorage.getItem('currentUser')), 'Perfil.sigla', 'guest');
 
     console.log('acl_unidades==>', localStorage.getItem('acl_unidades'))
 
     this.userUnidadesPath = {}; //FIX: Pega os acls
     //this.userUnidadesPath =  JSON.parse(localStorage.getItem('acl_unidades'));
-    this.userUnidadeCod = _.get(JSON.parse(localStorage.getItem('currentUser')),'UnidadeCodigo',false);
-    this.setRules(acl,role)
+    this.userUnidadeCod = _.get(JSON.parse(localStorage.getItem('currentUser')), 'UnidadeCodigo', false);
+    this.setRules(acl, role)
 
     //update with last rules
-    this.get('/acl-rules').subscribe(resp=>{
+    this.get('/acl-rules').subscribe(resp => {
       localStorage.setItem('acl', JSON.stringify(resp.acl_rules));
       localStorage.setItem('acl_unidades', JSON.stringify(resp.unidades));
       this.userUnidadesPath = resp.unidades
-      this.setRules(acl, role )
+      this.setRules(acl, role)
     });
 
   }
 
-  getAll(){
+  getAll() {
     return this.get('/acl-rules');
   }
 
-  public setRules(acl_rules, role ){
-     acl.config({
-        baseUrl: '',
-        rules:acl_rules,
+  public setRules(acl_rules, role) {
+    acl.config({
+      baseUrl: '',
+      rules: acl_rules,
     });
     this.role = role
   }
 
-  getPermission(route,method, unidadeCod = 0){
+  getPermission(route, method, unidadeCod = 0) {
 
     let options = acl.getConfig();
     const policy = options.policies.get(this.role);
     let access = acl.checkACL(this.role, route, method);
 
-    if(this.role == "ADM")
-        return access;
+    if (this.role == "ADM")
+      return access;
 
     //check if in $area, if it is, check permission for it
-    if(unidadeCod){
-        if( this.userUnidadesPath.includes( unidadeCod ) ){
-            //make
-            return true;
-        }else{
-            return false;
-        }
+    console.log("AHAHAHAHHAHA: ", this.userUnidadesPath)
+    if (unidadeCod) {
+      if (this.userUnidadesPath && this.userUnidadesPath.includes(unidadeCod)) {
+        //make
+        return true;
+      } else {
+        return false;
+      }
     }
 
     return access;
