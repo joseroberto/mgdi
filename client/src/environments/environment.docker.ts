0eb085d51f1398897c5d766ff81ddda81da78103
José Roberto <joserobertovasconcelos@gmail.com>
2023-06-15 01:56:07 -0300

    Configuracao de frontend

--- client/src/environments/environment.docker.ts
+++ client/src/environments/environment.docker.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   url: 'URL_REST',
-  API_URL: "CONTEXT_REST",
+  API_URL: "API_URL",
   aplicacao: "APP", // codigo da aplicacao SIGIN
   urlNewUser: "http://",
   urlForgetPassword: "http://",
   urlAuth: "URL_AUTH",
-  CONTEXT: "/mgdi"
+  CONTEXT: "CONTEXT"
 };
